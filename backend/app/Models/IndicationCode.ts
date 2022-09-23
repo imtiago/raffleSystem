e8a194464bb8f9445706a564dd0b79533ed02851
Tiago Oliveira <tiago.ufac@gmail.com>
2022-09-23 16:55:35 -0300

    was added the indication module

---
+++ backend/app/Models/IndicationCode.ts
@@ -0,0 +1,30 @@
+import {  BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import CamelCaseNamingStrategy from 'App/Strategies/CamelCaseNamingStrategy'
+import AppBaseModel from './AppBaseModel'
+import User from './User'
+
+
+export default class IndicationCode extends AppBaseModel {
+  public static selfAssignPrimaryKey = true
+  public static namingStrategy = new CamelCaseNamingStrategy()
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column({ serializeAs: null })
+  public indicationCodeReceived: string
+
+  @column()
+  public indicationCode: string
+  
+  @column()
+  public qrCode: string
+
+  @column()
+  public userId: string
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+}
---
+++ backend/database/migrations/1663899691945_indication_codes.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class IndicationCodes extends BaseSchema {
+  protected tableName = 'indication_codes'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('indication_code_received',50).nullable()
+      table.string('indication_code',50).notNullable()
+      table.string('qr_code').nullable()
+      table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE')
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
